e67635c8d8e70509eb288e849a89fd61a91bf80d
Hans Aeron Warde <hans.warde@circles.asia>
2023-03-29 20:42:31 +0800

    Remove hardcoded env variables

--- config.js
+++ config.js
@@ -2,12 +2,11 @@ const env = process.env;
 
 const config = {
   db: {
-    host:
-      env.DB_HOST || "ec2-54-169-182-174.ap-southeast-1.compute.amazonaws.com",
-    port: env.DB_PORT || "5432",
-    user: env.DB_USER || "applicant",
-    password: env.DB_PASSWORD || "OxzdeuEXBM85=+xQnCV7U",
-    database: env.DB_NAME || "FSD_2022_WARDE",
+    host: env.DB_HOST,
+    port: env.DB_PORT,
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
   },
 };
 
