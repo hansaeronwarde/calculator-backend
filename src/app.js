17297c21e0a2226bd29fb5c3aed63c46b0713c45
Hans Aeron Warde <hans.warde@circles.asia>
2023-03-30 21:21:59 +0800

    Add CORS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import routes from "./routes/routes.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
